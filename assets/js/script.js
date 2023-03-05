921fe70673a580ca1e506f76c78ddc9d88c4b3d0
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-03-05 02:01:01 -0500

    changed return statement of function to make it a little more usable

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,17 @@ const apiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 let history = [];
 
 //function that will return the latitude and longitude given a city name
-function getLoc(city){
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey)
+async function getLoc(city){
+    return fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey)
     .then((response) => response.json())
-
     .then((data) => {
-        console.log(data[0].lat + ", " + data[0].lon)
+        return {
+            "lat": data[0].lat,
+            "lon": data[0].lon
+        }
     });
 }
 
-getLoc("Columbus");
+getLoc("Columbus").then(coords => {
+    console.log(coords.lat + ", " + coords.lon);
+});
