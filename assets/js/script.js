3e4c82e94c01df5ef3f4079c13a427285fa996e1
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-03-05 22:37:43 -0500

    changed function to return current weather given city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
 const apiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 let history = [];
 
-//function that will return the latitude and longitude given a city name
-async function getLoc(city){
-    return fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey)
-    .then((response) => response.json())
-    .then((data) => {
-        return {
-            "lat": data[0].lat,
-            "lon": data[0].lon
-        }
+//function that will return the current weather given a city name
+function getWeatherCurrent(city){
+    //first passes the city name into a fetch to get the latitude and longitude of the city
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey)
+    .then(response => response.json())
+    .then(data => {
+        //uses the latitude and longitude in a fetch for the current weather
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&appid=" + apiKey)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
     });
 }
 
-getLoc("Columbus").then(coords => {
-    console.log(coords.lat + ", " + coords.lon);
-});
+// getWeatherCurrent("Columbus").then(coords => {
+//     console.log(coords.lat + ", " + coords.lon);
+// });
+
+getWeatherCurrent("Columbus");
